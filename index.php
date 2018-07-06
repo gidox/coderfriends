1272956412d99c7ad7e553d50fb90887ab79f85c
Arquimedes <guilandarquimedes@gmail.com>
2018-07-05 21:54:24 -0400

    done

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "coderfriends@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $subject = "Contacto desde el sitio WEb";
-    $subject2 = "Contacto desde el sitio WEb";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+if(isset($_POST['email'])){
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+	$to = "coderfriends@gmail.com"; // this is your Email address
+	$from = $_POST['email']; // this is the sender's Email address
+	$name = $_POST['name'];
+	$subject = "Contacto desde el sitio WEb";
+	$subject2 = "Contacto desde el sitio WEb";
+	$message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+	$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+	$headers = "From:" . $from;
+	$headers2 = "From:" . $to;
+	mail($to,$subject,$message,$headers);
+	mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+	// echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+	// You can also use header('Location: thank_you.php'); to redirect to another page.
+	header('Location: thank_you.php');
+}
 ?>
 
 <!DOCTYPE html>
